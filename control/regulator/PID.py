77b4b1d9aba4646e98efb1833556654adbd24722
astefanczyk <astefanczyk@student.agh.edu.pl>
2023-05-04 08:23:32 -0900

    complet PID regulation

--- control/regulator/PID.py
+++ control/regulator/PID.py
@@ -3,21 +3,31 @@ class PID:
     def __init__(self):
 
         self.P = -0.2
-        self.I = 0
+        self.I = 0.01
         self.D = 0.05
 
     def evaluate(self, input_value : dict) -> float:
 
-        diff = self.__get_diff(input_value=input_value)
-        output_value = (input_value['current']['x'] * self.P) + (diff['x'] * self.D)
+        diff_e = self.__get_diff_e(input_value=input_value)
+        sum_e = self.__get_sum_e(input_value=input_value)
+        output_value = (input_value['current']['x'] * self.P) + (diff_e['x'] * self.D) + (sum_e['x'] * self.I)
 
         return output_value
 
-    def __get_diff(self, input_value : dict) -> dict:
+    def __get_diff_e(self, input_value : dict) -> dict:
 
-        diff = {
+        diff_e = {
             'x' : input_value['current']['x'] - input_value['previous']['x'],
             'y' : input_value['current']['y'] - input_value['previous']['y']
         }
 
-        return diff
+        return diff_e
+
+    def __get_sum_e(self, input_value : dict) -> dict:
+
+        sum_e = {
+             'x' : input_value['current']['x'] + input_value['previous']['x'],
+             'y' : input_value['current']['y'] + input_value['previous']['y']
+         }
+
+        return sum_e
